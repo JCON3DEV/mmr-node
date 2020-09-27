5e2732320a7cf11af9120459bb91545a067670cf
Serah Sirohi <serahsirohi@gmail.com>
2020-09-27 22:33:44 +0000

    added events

--- db/seeds/05_events.sql
+++ db/seeds/05_events.sql
@@ -1,4 +1,8 @@
 INSERT INTO events
   (location,time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-  (abc);
+  ('Vancouver or Online','Chat with Marine Biologist','www.link.com',1,1);
+INSERT INTO events
+  (location,time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+  ('Vancouver or Online','Chat with Vet','www.link1.com',1,2);
