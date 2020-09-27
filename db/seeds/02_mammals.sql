afcc50ef14d64fa204690fa26e424c309d591917
Serah Sirohi <serahsirohi@gmail.com>
2020-09-27 22:12:51 +0000

    added mammals table

--- db/seeds/02_mammals.sql
+++ db/seeds/02_mammals.sql
@@ -2,7 +2,9 @@ INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic)
 VALUES
   ('Boris',4,120, 'Boris is the cutest','2016-06-22 19:10:25-07','2016-06-22 19:10:25-07','Boris.png, Boris1.png, Boris2.png');
--- INSERT INTO mammals
---   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic)
--- VALUES
---   ('Alfonso',2,50, "Alfonso is the cutest",1/4/2020, "-","imagepath");
+INSERT INTO mammals
+  (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic)
+VALUES
+  ('Boris',4,120, 'Boris is the cutest','2016-06-22 19:10:25-07','2016-06-22 19:10:25-07','Boris.png, Boris1.png, Boris2.png');
+
+
