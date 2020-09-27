781233b157cd8cf7f2eec57252a2c52a6b4d517d
JCON3DEV <jconheasman@gmail.com>
2020-09-27 04:39:00 +0000

    added the Alfonso experiment object

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,23 @@ const hits = [
   { objectID: 2, title: "Amazon", url: "http://amazon.ca" },
 ];
 
+const alfonso = [
+  {
+    mammal_id: 1,
+    mammal_name: "Alfonso",
+    age: 4,
+    weight: 6.2,
+    bio:
+      "Employees of the Mowi salmon farm spotted Alfonso swimming irregularly in waters near Hardwicke Island. She was observed unable to dive and floating sideways.",
+    date_admitted: "2020-12-25",
+    date_released: null,
+    profile_pic: "https://dqgt5bdmzyj47.cloudfront.net/wp-content/uploads/2020/07/DSC05570.jpg", //"docs/seals/alfonso.jpg",
+    event_id: 12,
+    admin_id: 1,
+    sponsored: true,
+  },
+];
+
 const express = require('express');
 const router  = express.Router();
 
@@ -27,7 +44,7 @@ module.exports = (db) => {
     //       .json({ error: err.message });
     //   });
 
-    res.json({hits});
+    res.json({ alfonso });
 
   });
   return router;
