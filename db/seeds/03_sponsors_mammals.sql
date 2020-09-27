69bcf8f4ca305705ba2bb9fadc7d774538119201
Serah Sirohi <serahsirohi@gmail.com>
2020-09-27 22:30:03 +0000

    added sponsors_mammals

---
+++ db/seeds/03_sponsors_mammals.sql
@@ -0,0 +1,33 @@
+-- INSERT INTO sponsors
+--   (sponsor_name, phone_number,email)
+-- VALUES
+--   ('Serah', 16041231234, 'serah@email.com');
+
+INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (1,1);
+  INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (1,2);
+INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (1,3);
+INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (1,4);
+INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (2,1);
+INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (2,1);
+INSERT INTO sponsors_mammals
+  (mammal_id,sponsor_id)
+VALUES
+  (2,1);
