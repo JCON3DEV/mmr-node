1e8fe5ac4bbc20983ff4cc300f87efaa15a20c88
JCON3DEV <jconheasman@gmail.com>
2020-09-29 05:19:17 +0000

    added the sponsored boolean to the value row of every Mammal seed.

--- db/seeds/02_mammals.sql
+++ db/seeds/02_mammals.sql
@@ -13,30 +13,30 @@ VALUES
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Blue',4,120, 'Blue is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Blue.png, Blue1.png, Blue2.png,Blue3.png, Blue4.png');
+  ('Blue',4,120, 'Blue is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Blue.png, Blue1.png, Blue2.png,Blue3.png, Blue4.png', false);
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Dr.Bonnie Herring',4,120, 'Dr.Bonnie Herring is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Dr.Bonnie Herring.png, Dr.Bonnie Herring1.png, Dr.Bonnie Herring2.png,Dr.Bonnie Herring3.png, Dr.Bonnie Herring4.png');
+  ('Dr.Bonnie Herring',4,120, 'Dr.Bonnie Herring is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Dr.Bonnie Herring.png, Dr.Bonnie Herring1.png, Dr.Bonnie Herring2.png,Dr.Bonnie Herring3.png, Dr.Bonnie Herring4.png', false);
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Focarelli',4,120, 'Focarelli is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Focarelli.png, Focarelli1.png, Focarelli2.png,Focarelli3.png, Focarelli4.png');
+  ('Focarelli',4,120, 'Focarelli is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Focarelli.png, Focarelli1.png, Focarelli2.png,Focarelli3.png, Focarelli4.png', false);
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Ona',4,120, 'Ona is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Ona.png, Ona1.png, Ona2.png,Ona3.png, Ona4.png');
+  ('Ona',4,120, 'Ona is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Ona.png, Ona1.png, Ona2.png,Ona3.png, Ona4.png',false);
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Penner',4,120, 'Penner is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Penner.png, Penner1.png, Penner2.png,Penner3.png, Penner4.png');
+  ('Penner',4,120, 'Penner is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Penner.png, Penner1.png, Penner2.png,Penner3.png, Penner4.png', true);
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Saucy',4,120, 'Saucy is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Saucy.png, Saucy1.png, Saucy2.png,Saucy3.png, Saucy4.png');
+  ('Saucy',4,120, 'Saucy is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Saucy.png, Saucy1.png, Saucy2.png,Saucy3.png, Saucy4.png', false);
 INSERT INTO mammals
   (mammal_name,age,weight,bio,date_admitted,date_released,profile_pic, sponsored)
 VALUES
-  ('Sealy',4,120, 'Sealy is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Sealy.png, Sealy1.png, Sealy2.png,Sealy3.png, Sealy4.png');
+  ('Sealy',4,120, 'Sealy is the cutest','2016-06-22 19:10:25-07','2016-09-01 20:15:20-00','Sealy.png, Sealy1.png, Sealy2.png,Sealy3.png, Sealy4.png', false);
 
 
