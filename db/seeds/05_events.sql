c2281e1df9855163a8d760f5b9710b706d9910e3
JCON3DEV <jconheasman@gmail.com>
2020-10-02 17:36:14 +0000

    added some more events to the DB.

--- db/seeds/05_events.sql
+++ db/seeds/05_events.sql
@@ -2,7 +2,48 @@ INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
   ('Vancouver or Online','2020-10-15 16:15:20-00','Chat with Marine Biologist','www.link.com',1,1,1);
+
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
    ('Vancouver or Online','2020-10-20 16:00:20-00','Chat with Vet','www.link.com',2,2,2);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Gar Point Park or Online','2020-10-08 10:00:16-00','Litter pick around the coastline','www.cleanbeaches.com',0,2,3);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Online','2020-10-24 11:00:12-00','Feeding and medications','www.videolink.com',4,1,2);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Online','2020-10-24 12:00:13-00','Play time and microchipping','www.videolink.com',1,1,3);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Alaskan National Wildlife Area or Online','2020-10-24 13:00:16-00','Mammal watch. Coastline conservation','www.cleanbeaches.com',0,2,1);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Jericho Beach Park or Online','2020-11-07 11:00:16-00','Beach Clean-up. Coastline conservation','www.cleanbeaches.com',0,1,4);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Online','2020-10-14 11:00:12-00','Feeding and medications','www.videolink.com',1,1,1);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Online','2020-10-27 11:00:12-00','Feeding and medications','www.videolink.com',4,1,1);
+
+INSERT INTO events
+  (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
+VALUES
+   ('Online','2020-11-03 11:00:12-00','Feeding and medications','www.videolink.com',4,1,1);
