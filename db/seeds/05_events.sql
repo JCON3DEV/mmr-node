37fa49e1cabb088e473d32351570643199fccc7a
Serah Sirohi <serahsirohi@gmail.com>
2020-10-03 03:47:26 +0000

    added a few unique links for events

--- db/seeds/05_events.sql
+++ db/seeds/05_events.sql
@@ -8,19 +8,19 @@ VALUES
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Vancouver or Online','2020-10-20 16:00:20-00','Chat with Vet','www.link.com',2,2,2);
+   ('Vancouver or Online','2020-10-20 16:00:20-00','Chat with Vet','https://www.youtube.com/embed/1tJBE9j1WWc',2,2,2);
 
 -- ev3
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Gar Point Park or Online','2020-10-08 10:00:16-00','Litter pick around the coastline','www.cleanbeaches.com',null,2,3);
+   ('Gar Point Park or Online','2020-10-08 10:00:16-00','Litter pick around the coastline','https://www.youtube.com/embed/DDLCBm-gWZ8',null,2,3);
 
 -- ev4
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-24 11:00:12-00','Feeding and medications','www.videolink.com',4,1,2);
+   ('Online','2020-10-24 11:00:12-00','Seal Release','https://www.youtube.com/embed/1tJBE9j1WWc',4,1,2);
 
 -- ev5
 INSERT INTO events
@@ -32,28 +32,28 @@ VALUES
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Alaskan National Wildlife Area or Online','2020-10-24 13:00:16-00','Mammal watch. Coastline conservation','www.cleanbeaches.com',null,2,1);
+   ('Alaskan National Wildlife Area or Online','2020-10-24 13:00:16-00','Mammal watch. Coastline conservation','https://www.youtube.com/embed/aLj1eTLIeCA',null,2,1);
 
 -- ev7
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Jericho Beach Park or Online','2020-11-07 11:00:16-00','Beach Clean-up. Coastline conservation','www.cleanbeaches.com',null,1,4);
+   ('Jericho Beach Park or Online','2020-11-07 11:00:16-00','Beach Clean-up. Coastline conservation','https://www.youtube.com/embed/nA07MIFXdoY',null,1,4);
 
 -- ev8
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-14 11:00:12-00','Feeding and medications','www.videolink.com',1,1,1);
+   ('Online','2020-10-14 11:00:12-00','Feeding and medications','https://www.youtube.com/embed/3GcdoKwqJNA',1,1,1);
 
 -- ev9
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-27 11:00:12-00','Feeding and medications','www.videolink.com',4,1,1);
+   ('Online','2020-10-27 11:00:12-00','Seal Pups','https://www.youtube.com/embed/bqMyiqRunAU',4,1,1);
 
 -- ev10
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-11-03 11:00:12-00','Feeding and medications','www.videolink.com',4,1,1);
+   ('Online','2020-11-03 11:00:12-00','Feeding and medications','https://www.youtube.com/embed/3GcdoKwqJNA',4,1,1);
