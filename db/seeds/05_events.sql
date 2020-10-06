a513ba0b5c6009d39044cd37186356014edade3b
JCON3DEV <jconheasman@gmail.com>
2020-10-06 19:17:21 +0000

    ensured all videos are unique. need to ensure release vids are not associated with current mammals.

--- db/seeds/05_events.sql
+++ db/seeds/05_events.sql
@@ -2,103 +2,108 @@
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-  ('Vancouver or Online','2020-10-15 16:15:20-00','Chat with Marine Biologist','https://www.youtube.com/embed/xV2zvZDVw4g',1,1,1);
+  ('Vancouver or Online','2020-10-15 16:15:20-00','Check up with Blue','https://www.youtube.com/embed/xV2zvZDVw4g',5,1,1);
 
--- ev2 // Seal Release from Porteau Cove
+-- ev2 // Seal pup bottle feeding (cute)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Vancouver or Online','2020-10-20 16:00:20-00','Chat with Vet','https://www.youtube.com/embed/1tJBE9j1WWc',2,2,2);
+   ('Vancouver or Online','2020-10-20 16:00:20-00','Bottle feeding the new addition','https://www.youtube.com/embed/ygXNG9bEKms', 2,2,2);
 
--- ev3 // Rescue Rehabilitation and release Marine mamaml centre
+-- ev3  -- christmas 2019 beach clean
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Gar Point Park or Online','2020-10-08 10:00:16-00','Litter pick around the coastline','https://www.youtube.com/embed/DDLCBm-gWZ8',null,2,3);
+   ('Gar Point Park or Online','2020-10-08 10:00:16-00','Litter pick around the coastline','https://www.youtube.com/embed/TVLUoyTEl6s',null,2,3);
 
--- ev4 // Seal Release from Porteau Cove
+-- ev4 // Seal Release from Porteau Cove (cute, excellent)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-24 11:00:12-00','Seal Release','https://www.youtube.com/embed/1tJBE9j1WWc',4,1,2);
+   ('Online','2020-10-24 11:00:12-00','Seal Release','https://www.youtube.com/embed/1tJBE9j1WWc',7,1,2);
 
--- ev5 // Joey doesnt like water (yet)
+-- ev5 // Joey doesnt like water (yet) (wrong animal but very cute)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
    ('Online','2020-10-24 12:00:13-00','Play time and microchipping','https://www.youtube.com/embed/0DMX4IF-1A4',1,1,3);
 
--- ev6 // Dawn and Cartilla in their training pool
+-- ev6 // Dawn and Cartilla in their training pool (cute excellent)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Alaskan National Wildlife Area or Online','2020-10-24 13:00:16-00','Mammal watch. Coastline conservation','https://www.youtube.com/embed/aLj1eTLIeCA',null,2,1);
+   ('Alaskan National Wildlife Area or Online','2020-10-24 13:00:16-00','Social time for our guests','https://www.youtube.com/embed/aLj1eTLIeCA',5,2,1);
 
--- ev7 // Yuki relaxing after his Lunch feed
+-- ev7 // Yuki relaxing after his Lunch feed (mega cute)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Jericho Beach Park or Online','2020-11-07 11:00:16-00','Beach Clean-up. Coastline conservation','https://www.youtube.com/embed/nA07MIFXdoY',null,1,4);
+   ('Jericho Beach Park or Online','2020-11-07 11:00:16-00','Seal Pup Playtime','https://www.youtube.com/embed/nA07MIFXdoY',null,1,4);
 
--- ev8
+-- ev8 // Joey sea otte meeting friends - wrong animal but acceptable
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
    ('Online','2020-10-14 11:00:12-00','Feeding and medications','https://www.youtube.com/embed/3GcdoKwqJNA',1,1,1);
 
--- ev9 // Seal pups and otters at MMR
+-- ev9 // Seal pups and otters at MMR (great "live" footage chat with vets)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-27 11:00:12-00','Seal Pup play time','https://www.youtube.com/embed/bqMyiqRunAU',4,1,1);
+   ('Online','2020-10-27 11:00:12-00','Chat with Vets','https://www.youtube.com/embed/bqMyiqRunAU',4,1,1);
 
--- ev10 // Joey (the sea otter) meets new friends
+-- ev10 // vet check from Aukland zoo
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-11-03 11:00:12-00','Feeding and medications','https://www.youtube.com/embed/3GcdoKwqJNA',4,1,1);
+   ('Online','2020-11-03 11:00:12-00','Feeding and medications with the Vet','https://www.youtube.com/embed/Nch4Sv6Qi_w',4,1,1);
 
--- ev11 // Joey doesnt like water (yet)
+-- ev11 // Taronga Zoo's Leopard Seal Serenaded by a Saxophone
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-18 12:00:13-00','Play time and vaccination','https://www.youtube.com/embed/0DMX4IF-1A4',3,1,3);
+   ('Online','2020-10-18 12:00:13-00','Jazz Music for our seals','https://www.youtube.com/embed/EgRq7wKkRp0',3,1,3);
 
--- ev12 // Seal pup bottle feeding. (super cute)
+-- ev12 // Seals being fed -Detroit zoo
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-26 12:00:13-00','Feeding and progress check','https://www.youtube.com/watch?v=ygXNG9bEKms',5,2,4);
+   ('Online','2020-10-26 12:00:13-00','Feeding and progress check','https://www.youtube.com/embed/GPLekdx0uME',5,2,4);
 
--- ev13 // two grey seal pups born at brockfield zoo
+-- ev13 // two grey seal pups born at brockfield zoo (cool)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-29 12:00:13-00','Physio therapy and play','https://www.youtube.com/watch?v=kmKDCXcd52s',6,2,5);
+   ('Online','2020-10-29 12:00:13-00','Physio therapy and play','https://www.youtube.com/embed/kmKDCXcd52s',6,2,5);
 
--- ev14 // Seal Lion Gets Medical Checkup
+-- ev14 // Seal Lion Gets Medical Checkup (cute, spot on)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-10-18 12:00:13-00','Medical check up and treats','https://www.youtube.com/watch?v=M9iV9KJVPys',7,3,1);
+   ('Online','2020-10-18 12:00:13-00','Medical check up and treats','https://www.youtube.com/embed/M9iV9KJVPys',7,3,1);
 
 -- ev15 // Baby Seal Boy "Ark" Arrives at the Rescue Centre (super cute)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Online','2020-11-17 12:00:13-00','Feeding and medications','https://www.youtube.com/watch?v=KfxVSuC_ubU',8,4,1);
+   ('Online','2020-11-17 12:00:13-00','Feeding and medications','https://www.youtube.com/embed/KfxVSuC_ubU',8,4,1);
 
--- ev16 // harbour seal;s released back into the wild MMR
+-- ev16 // harbour seals released back into the wild MMR (great cute)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Alaskan National Wildlife Area or Online','2020-11-14 12:00:13-00','Release Day','https://www.youtube.com/watch?v=eDBJuJjma14',9,3,2);
+   ('Alaskan National Wildlife Area or Online','2020-11-14 12:00:13-00','Release Day','https://www.youtube.com/embed/eDBJuJjma14',9,3,2);
 
 -- ev17 Seal release (New haven register)
 INSERT INTO events
   (location,date_time,short_description,link,mammal_id,sponsor_id,veterinarian_id)
 VALUES
-   ('Jericho Beach Park or Online','2020-11-28 12:00:13-00','Release Day','https://www.youtube.com/watch?v=bO-WZdpDwUI',10,3,2);
--- vet check from Aukland zoo    https://www.youtube.com/watch?v=Nch4Sv6Qi_w
+   ('Jericho Beach Park or Online','2020-11-28 12:00:13-00','Release Day','https://www.youtube.com/embed/bO-WZdpDwUI',10,3,2);
 
--- christmas 2019 beach clean    https://www.youtube.com/watch?v=TVLUoyTEl6s
+-- // Rescue Rehabilitation and release Marine mamaml centre (not live, logo intro) https://www.youtube.com/embed/DDLCBm-gWZ8
+
+-- Seal Release from Porteau Cove https://www.youtube.com/embed/1tJBE9j1WWc
+
+
+-- Mammal watch. Coastline conservation
+-- Beach Clean-up. Coastline conservation
